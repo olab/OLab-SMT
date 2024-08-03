356a1d9e68112c97679eaab575fd57a3afde83c6
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2024-08-03 10:38:49 -0600

    initial code load

---
+++ src/components/HomeLayout.jsx
@@ -0,0 +1,21 @@
+import { Navigate, useOutlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { AppBar } from "./AppBar";
+
+export const HomeLayout = () => {
+  const { user } = useAuth();
+  const outlet = useOutlet();
+
+  if (user) {
+    return <Navigate to="/dashboard/profile" replace />;
+  }
+
+  return (
+    <div>
+      <AppBar
+        pages={[]}
+      />
+      {outlet}
+    </div>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./App";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#3a34d2" }
+  }
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
