95881d2c9c182c40c7233e8ee678c2c869cbda27
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2024-08-17 12:56:26 -0600

    .env work

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: true
-  },
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
+export default defineConfig(({ command, mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+   base: `${env.VITE_APP_BASEPATH}`,
+    build: {
+      sourcemap: true
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+      ],
+    }
+  }
+
 })
