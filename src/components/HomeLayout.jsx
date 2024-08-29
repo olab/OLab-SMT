4c30d0132472b6eecb3e23854fdd1f1860aeef7e
Corey Wirun <reg@pathrider.ca>
2024-08-29 08:02:19 -0600

    fix: fixed route in homelayout

--- src/components/HomeLayout.jsx
+++ src/components/HomeLayout.jsx
@@ -1,13 +1,14 @@
 import { Navigate, useOutlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import { AppBar } from "./AppBar";
+import { config } from "../config";
 
 export const HomeLayout = () => {
   const { user } = useAuth();
   const outlet = useOutlet();
 
   if (user) {
-    return <Navigate to="/dashboard/home" replace />;
+    return <Navigate to={`${config.APP_BASEPATH}/dashboard/home`} replace />;
   }
 
   return (
