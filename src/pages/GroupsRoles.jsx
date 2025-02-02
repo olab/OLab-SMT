8065b315feb8b249833aec0080deefe8f216f132
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-02-02 08:59:32 -0700

    under construction page for groups/roles

--- src/pages/GroupsRoles.jsx
+++ src/pages/GroupsRoles.jsx
@@ -2,5 +2,5 @@ import { BasicPage } from "../components/BasicPage";
 import Person from "@mui/icons-material/Person";
 
 export const GroupsRolesPage = () => {
-  return <BasicPage title="Profile Page" icon={<Person />} />;
+  return <BasicPage title="Under construction" icon={<Person />} />;
 };
